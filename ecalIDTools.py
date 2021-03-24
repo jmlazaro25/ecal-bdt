99e1926d9845299307d42ae8917fc9b88cb3a656
Anthony Emilio Chavez <aechavez@cent7b.slac.stanford.edu>
2021-03-24 01:45:03 -0700

    Committer: Anthony Emilio Chavez <aechavez@cent7b.slac.stanford.edu>  On branch master  Changes to be committed: 	new file:   ecalHexReadoutTools.py 	new file:   ecalIDTools.py 	deleted:    ECalVetoAnalyzer.cxx 	deleted:    EcalHexReadoutTools.py 	deleted:    EcalIDTools.py 	deleted:    ROOTmanager.pyc 	deleted:    hi.py 	deleted:    physTools.pyc

---
+++ ecalIDTools.py
@@ -0,0 +1,55 @@
+LAYER_MASK = 0x3F
+LAYER_SHIFT = 17
+MODULE_MASK = 0x1F
+MODULE_SHIFT = 12
+CELL_MASK = 0xFFF
+CELL_SHIFT = 0
+
+######################################
+# Hit information from hitID
+######################################
+
+# Function to extract the layer from a hitID
+def IDToLayer(hitID):
+    return (hitID>>LAYER_SHIFT)&LAYER_MASK
+
+# Function to extract the module from a hitID
+def IDToModule(hitID):
+    return (hitID>>MODULE_SHIFT)&MODULE_MASK
+
+# Function to extract the cell from a hitID
+def IDToCell(hitID):
+    return (hitID>>CELL_SHIFT)&CELL_MASK
+
+################################################
+# Naive implementation of EcalID class
+################################################
+
+class EcalID:
+
+    def __init__(self, layer, module, cell):
+
+        self.layer = layer
+        self.module = module
+        self.cell = cell
+
+    def setLayer(self, newLayer):
+        return self.layer = newLayer
+
+    def getLayer(self):
+        return self.layer
+
+    def setModule(self, newModule):
+        return self.module = newModule
+
+    def getModule(self):
+        return self.module
+
+    def setCell(self, newCell):
+        return self.cell = newCell
+
+    def getCell(self):
+        return self.cell
+
+def emptyEcalID():
+    return EcalID(None, None, None)
