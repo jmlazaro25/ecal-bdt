2f4e518328f53f772eed1a71e3c215ce42b624ff
Juan Lazaro <juan.lazaro2599@gmail.com>
2021-03-28 19:08:58 -0700

    bdtMaker and Eval formatting and NEW STRUCTURE in Eval

--- bdtEval.py
+++ bdtEval.py
@@ -1,155 +1,10 @@
-#!/usr/bin/python
-import argparse
 import os
 import sys
-import ROOT as r
-import xgboost as xgb
-import pickle as pkl
 import numpy as np
+import pickle as pkl
+import xgboost as xgb
 import ROOTmanager as manager
-from collections import OrderedDict
 
-
-# TreeModel to build here
-branches_info = {
-        'nHits':      {'rtype': float, 'default': 0.},
-        'centE':      {'rtype': float, 'default': 0.},
-        'centPE':     {'rtype': float, 'default': 0.},
-        'std_e_z':    {'rtype': float, 'default': 0.},
-        'totE':       {'rtype': float, 'default': 0.},
-        'totPE':      {'rtype': float, 'default': 0.},
-        'maxE':       {'rtype': float, 'default': 0.},
-        'maxPE':      {'rtype': float, 'default': 0.},
-        'dz_e':       {'rtype': float, 'default': 0.},
-
-        'std_e_1_e':  {'rtype': float, 'default': 0.},
-        'std_e_1_r':  {'rtype': float, 'default': 0.},
-        'std_e_1_pe': {'rtype': float, 'default': 0.},
-        'avg_e_1_e':  {'rtype': float, 'default': 0.},
-        'avg_e_1_pe': {'rtype': float, 'default': 0.},
-        'tot_e_1_e':  {'rtype': float, 'default': 0.},
-        'tot_e_1_pe': {'rtype': float, 'default': 0.},
-        'max_e_1_e':  {'rtype': float, 'default': 0.},
-        'max_e_1_pe': {'rtype': float, 'default': 0.},
-
-        'std_e_2_e':  {'rtype': float, 'default': 0.},
-        'std_e_2_r':  {'rtype': float, 'default': 0.},
-        'std_e_2_pe': {'rtype': float, 'default': 0.},
-        'avg_e_2_e':  {'rtype': float, 'default': 0.},
-        'avg_e_2_pe': {'rtype': float, 'default': 0.},
-        'tot_e_2_e':  {'rtype': float, 'default': 0.},
-        'tot_e_2_pe': {'rtype': float, 'default': 0.},
-        'max_e_2_e':  {'rtype': float, 'default': 0.},
-        'max_e_2_pe': {'rtype': float, 'default': 0.},
-
-        'std_e_3_e':  {'rtype': float, 'default': 0.},
-        'std_e_3_r':  {'rtype': float, 'default': 0.},
-        'std_e_3_pe': {'rtype': float, 'default': 0.},
-        'avg_e_3_e':  {'rtype': float, 'default': 0.},
-        'avg_e_3_pe': {'rtype': float, 'default': 0.},
-        'tot_e_3_e':  {'rtype': float, 'default': 0.},
-        'tot_e_3_pe': {'rtype': float, 'default': 0.},
-        'max_e_3_e':  {'rtype': float, 'default': 0.},
-        'max_e_3_pe': {'rtype': float, 'default': 0.},
-
-        'discValue_HcalVeto': {'rtype': float, 'default': 0.5}
-        }
-
-def event_process(self):
-   
-    # Reset
-    self.tfMaker.resetBranches()
-
-    # Collect info for prediction
-    evtarray = np.array([[
-          self.tree.nHits,
-          self.tree.centE,
-          self.tree.centPE,
-          self.tree.std_e_z,
-          self.tree.totE,
-          self.tree.totPE,
-          self.tree.maxE,
-          self.tree.maxPE,
-          self.tree.dz_e,
-                               
-          self.tree.std_e_1_e,
-          self.tree.std_e_1_r,
-          self.tree.std_e_1_pe,
-          self.tree.avg_e_1_e,
-          self.tree.avg_e_1_pe,
-          self.tree.tot_e_1_e,
-          self.tree.tot_e_1_pe,
-          self.tree.max_e_1_e,
-          self.tree.max_e_1_pe,
-                               
-          self.tree.std_e_2_e,
-          self.tree.std_e_2_r,
-          self.tree.std_e_2_pe,
-          self.tree.avg_e_2_e,
-          self.tree.avg_e_2_pe,
-          self.tree.tot_e_2_e,
-          self.tree.tot_e_2_pe,
-          self.tree.max_e_2_e,
-          self.tree.max_e_2_pe,
-                               
-          self.tree.std_e_3_e,
-          self.tree.std_e_3_r,
-          self.tree.std_e_3_pe,
-          self.tree.avg_e_3_e,
-          self.tree.avg_e_3_pe,
-          self.tree.tot_e_3_e,
-          self.tree.tot_e_3_pe,
-          self.tree.max_e_3_e,
-          self.tree.max_e_3_pe
-          ]])
-
-    # Copy inputs
-    self.tfMaker.branches['nHits'][0]           = self.tree.nHits
-    self.tfMaker.branches['centE'][0]           = self.tree.centE
-    self.tfMaker.branches['centPE'][0]          = self.tree.centPE
-    self.tfMaker.branches['std_e_z'][0]         = self.tree.std_e_z
-    self.tfMaker.branches['totE'][0]            = self.tree.totE
-    self.tfMaker.branches['totPE'][0]           = self.tree.totPE
-    self.tfMaker.branches['maxE'][0]            = self.tree.maxE
-    self.tfMaker.branches['maxPE'][0]           = self.tree.maxPE
-    self.tfMaker.branches['dz_e'][0]            = self.tree.dz_e
-
-    self.tfMaker.branches['std_e_1_e'][0]       = self.tree.std_e_1_e
-    self.tfMaker.branches['std_e_1_r'][0]       = self.tree.std_e_1_r
-    self.tfMaker.branches['std_e_1_pe'][0]      = self.tree.std_e_1_pe
-    self.tfMaker.branches['avg_e_1_e'][0]       = self.tree.avg_e_1_e
-    self.tfMaker.branches['avg_e_1_pe'][0]      = self.tree.avg_e_1_pe
-    self.tfMaker.branches['tot_e_1_e'][0]       = self.tree.tot_e_1_e
-    self.tfMaker.branches['tot_e_1_pe'][0]      = self.tree.tot_e_1_pe
-    self.tfMaker.branches['max_e_1_e'][0]       = self.tree.max_e_1_e
-    self.tfMaker.branches['max_e_1_pe'][0]      = self.tree.max_e_1_pe
-
-    self.tfMaker.branches['std_e_2_e'][0]       = self.tree.std_e_2_e
-    self.tfMaker.branches['std_e_2_r'][0]       = self.tree.std_e_2_r
-    self.tfMaker.branches['std_e_2_pe'][0]      = self.tree.std_e_2_pe
-    self.tfMaker.branches['avg_e_2_e'][0]       = self.tree.avg_e_2_e
-    self.tfMaker.branches['avg_e_2_pe'][0]      = self.tree.avg_e_2_pe
-    self.tfMaker.branches['tot_e_2_e'][0]       = self.tree.tot_e_2_e
-    self.tfMaker.branches['tot_e_2_pe'][0]      = self.tree.tot_e_2_pe
-    self.tfMaker.branches['max_e_2_e'][0]       = self.tree.max_e_2_e
-    self.tfMaker.branches['max_e_2_pe'][0]      = self.tree.max_e_2_pe
-
-    self.tfMaker.branches['std_e_3_e'][0]       = self.tree.std_e_3_e
-    self.tfMaker.branches['std_e_3_r'][0]       = self.tree.std_e_3_r
-    self.tfMaker.branches['std_e_3_pe'][0]      = self.tree.std_e_3_pe
-    self.tfMaker.branches['avg_e_3_e'][0]       = self.tree.avg_e_3_e
-    self.tfMaker.branches['avg_e_3_pe'][0]      = self.tree.avg_e_3_pe
-    self.tfMaker.branches['tot_e_3_e'][0]       = self.tree.tot_e_3_e
-    self.tfMaker.branches['tot_e_3_pe'][0]      = self.tree.tot_e_3_pe
-    self.tfMaker.branches['max_e_3_e'][0]       = self.tree.max_e_3_e
-    self.tfMaker.branches['max_e_3_pe'][0]      = self.tree.max_e_3_pe
-
-    #evtarray = np.array([evt]) Add prediction
-    pred = float(self.model.predict(xgb.DMatrix(evtarray))[0])
-    self.tfMaker.branches['discValue_HcalVeto'][0] = pred
-
-    # Fill new tree with current event values
-    self.tfMaker.tree.Fill()
 
 def main():
 
@@ -162,18 +17,48 @@ def main():
     cwd = os.getcwd()
     pkl_file = cwd+'/bdt_0/bdt_0_weights.pkl'
 
+    # TreeModel to build here
+    # Note: except for the discValue, defaults don't really matter here
+    branches_info = {
+        'nReadoutHits':         {'rtype': int, 'default': 0.},
+        'nStraightTracks':      {'rtype': int, 'default': 0.},
+
+        'discValue_HcalVeto':   {'rtype': float, 'default': 0.5}
+        }
+
     # Make a process
-    proc = manager.TreeProcess(event_process, inlist[0], tree_name='HcalVeto', ID='onlyProc')
+    proc = manager.TreeProcess(event_process, inlist[0], tree_name='EcalVeto', ID='onlyProc')
     proc.model = pkl.load(open(pkl_file,'rb'))
     
     # Make an output file and new tree (copied from input + discValue)
     proc.tfMaker = manager.TreeMaker(outlist[0], "HcalVeto", branches_info)
    
     # RUN
-    proc.extraf = proc.tfMaker.wq # gets executed at the end of run()
+    proc.extraf = proc.tfMaker.wq # Gets executed at the end of run()
     proc.run()
 
     print('\nDone!\n')
+
+def event_process(self):
+
+    # Feature list from input tree
+    # Exp: feats = [ feat_value for feat_value in self.tree ]
+    feats = [
+          self.tree.nReadoutHits,
+          self.tree.nStraightTracks
+          ]
+
+    # Copy input tree feats to new tree
+    for feat_name, feat_value in zip(self.tfMaker.branches_info, feats):
+        self.tfMaker.branches[feat][0] = feat_value
+
+    # Add prediction to new tree
+    evtarray = np.array([feats])
+    pred = float(self.model.predict(xgb.DMatrix(evtarray))[0])
+    self.tfMaker.branches['discValue_HcalVeto'][0] = pred
+
+    # Fill new tree with current event values
+    self.tfMaker.tree.Fill()
 
 if __name__ == "__main__":
     main()
--- bdtMaker.py
+++ bdtMaker.py
@@ -1,39 +1,33 @@
-#!/usr/bin/python
-
 import os
 import sys
-import math
-import random
 import logging
 import argparse
-import importlib
 import ROOT as r
 import numpy as np
 import pickle as pkl
 import xgboost as xgb
 import matplotlib as plt
+from array    import array
+from optparse import OptionParser
 
-#logger = logging.getLogger(__name__)
-#logger.setLevel(logging.DEBUG)
 
 mpl_logger = logging.getLogger('matplotlib')
 mpl_logger.setLevel(logging.WARNING)
-
 plt.use('Agg')
-from collections import Counter
-from array import array
-from optparse import OptionParser
 sys.path.insert(0, '../')
-from sklearn import metrics
+
+# Because parsing flags can be annoying sometimes
+bkg_file = 'bdt_0/bkg_train.root'
+sig_file = 'bdt_0/sig_train.root'
 
 class sampleContainer:
     def __init__(self,filename,maxEvts,isSig):
-        print("Initializing Container!")
-        self.tree = r.TChain("HcalVeto")
-        self.tree.Add(filename)
 
-        self.maxEvts   = maxEvts
-        self.isSig = isSig
+        print("Initializing Container!")
+        self.tree = r.TChain("EcalVeto")
+        self.tree.Add(filename)
+        self.maxEvts = maxEvts
+        self.isSig   = isSig
 
     def root2PyEvents(self):
         self.events =  []
@@ -45,45 +39,8 @@ class sampleContainer:
             ###################################
             # Features
             ###################################
-            evt.append(event.nHits)      # 0
-            evt.append(event.centE)      # 1
-            evt.append(event.centPE)     # 2
-            evt.append(event.std_e_z)    # 3
-            evt.append(event.totE)       # 4
-            evt.append(event.totPE)      # 5
-            evt.append(event.maxE)       # 6
-            evt.append(event.maxPE)      # 7
-            evt.append(event.dz_e)       # 8
-
-            evt.append(event.std_e_1_r)  # 9
-            evt.append(event.std_e_1_e)  # 10
-            evt.append(event.std_e_1_pe) # 11
-            evt.append(event.avg_e_1_e)  # 12
-            evt.append(event.avg_e_1_pe) # 13
-            evt.append(event.tot_e_1_e)  # 14
-            evt.append(event.tot_e_1_pe) # 15
-            evt.append(event.max_e_1_e)  # 16
-            evt.append(event.max_e_1_pe) # 17
-
-            evt.append(event.std_e_2_r)  # 18
-            evt.append(event.std_e_2_e)  # 19
-            evt.append(event.std_e_2_pe) # 20
-            evt.append(event.avg_e_2_e)  # 21
-            evt.append(event.avg_e_2_pe) # 22
-            evt.append(event.tot_e_2_e)  # 23
-            evt.append(event.tot_e_2_pe) # 24
-            evt.append(event.max_e_2_e)  # 25
-            evt.append(event.max_e_2_pe) # 26
-
-            evt.append(event.std_e_3_r)  # 27
-            evt.append(event.std_e_3_e)  # 28
-            evt.append(event.std_e_3_pe) # 29
-            evt.append(event.avg_e_3_e)  # 30
-            evt.append(event.avg_e_3_pe) # 31
-            evt.append(event.tot_e_3_e)  # 32
-            evt.append(event.tot_e_3_pe) # 33
-            evt.append(event.max_e_3_e)  # 34
-            evt.append(event.max_e_3_pe) # 35
+            evt.append(self.nReadoutHits)       # 0
+            evt.append(self.nStraightTracks)    # 1
 
             self.events.append(evt)
 
@@ -110,75 +67,83 @@ class mergedContainer:
 
 if __name__ == "__main__":
     
+    # Parse
     parser = OptionParser()
-
-
     parser.add_option('--seed', dest='seed',type="int",  default=2, help='Numpy random seed.')
     parser.add_option('--max_evt', dest='max_evt',type="int",  default=1500000, help='Max Events to load')
     parser.add_option('--out_name', dest='out_name',  default='test', help='Output Pickle Name')
     parser.add_option('--eta', dest='eta',type="float",  default=0.023, help='Learning Rate')
     parser.add_option('--tree_number', dest='tree_number',type="int",  default=1000, help='Tree Number')
     parser.add_option('--depth', dest='depth',type="int",  default=10, help='Max Tree Depth')
-    parser.add_option('--bkg_file', dest='bkg_file', default='bdt_0/bkg_train.root', help='name of background file')
-    parser.add_option('--sig_file', dest='sig_file', default='bdt_0/sig_train.root', help='name of signal file')
-  
-
+    parser.add_option('--bkg_file', dest='bkg_file', default=bkg_file, help='name of background file')
+    parser.add_option('--sig_file', dest='sig_file', default=sig_file, help='name of signal file')
     (options, args) = parser.parse_args()
 
+    # Seed numpy's randomness
     np.random.seed(options.seed)
-    
-    adds=0
+   
+    # Get BDT num
+    bdt_num=0
     Check=True
     while Check:
-        if not os.path.exists(options.out_name+'_'+str(adds)):
+        if not os.path.exists(options.out_name+'_'+str(bdt_num)):
             try:
-                os.makedirs(options.out_name+'_'+str(adds))
+                os.makedirs(options.out_name+'_'+str(bdt_num))
                 Check=False
             except:
                Check=True
         else:
-            adds+=1
+            bdt_num+=1
 
+    # Print run info
+    print( 'Random seed is = {}'.format(options.seed)             )
+    print( 'You set max_evt = {}'.format(options.max_evt)         )
+    print( 'You set tree number = {}'.format(options.tree_number) )
+    print( 'You set max tree depth = {}'.format(options.depth)    )
+    print( 'You set eta = {}'.format(options.eta)                 )
 
-    print("Random seed is = %s" % (options.seed))
-    print("You set max_evt = %s" % (options.max_evt))
-    print("You set tree number = %s" % (options.tree_number))
-    print("You set max tree depth = %s" % (options.depth))
-    print("You set eta = %s" % (options.eta))
-
-    print('Loading sig_file = %s' % (options.sig_file))
+    # Make Signal Container
+    print( 'Loading sig_file = .format{}'.format(options.sig_file) )
     sigContainer = sampleContainer(options.sig_file,options.max_evt,True)
     sigContainer.root2PyEvents()
     sigContainer.constructTrainAndTest()
 
-    print('Loading bkg_file = %s' % (options.bkg_file))
+    # Make Background Container
+    print( 'Loading bkg_file = %s'.format(options.bkg_file) )
     bkgContainer = sampleContainer(options.bkg_file,options.max_evt,False)
     bkgContainer.root2PyEvents()
     bkgContainer.constructTrainAndTest()
 
+    # Merge
     eventContainer = mergedContainer(sigContainer,bkgContainer)
 
-    params     = {"objective": "binary:logistic",
-                "eta": options.eta,
-                "max_depth": options.depth,
-                "min_child_weigrt": 20,
-                "silent": 1,
-                "subsample": .9,
-                "colsample_bytree": .85,
-                #"eval_metric": 'auc',
-                "eval_metric": 'error',
-                "seed": 1,
-                "nthread": 1,
-                "verbosity": 1,
-                "early_stopping_rounds" : 10}
+    params     = {'objective': 'binary:logistic',
+                  'eta': options.eta,
+                  'max_depth': options.depth,
+                  'min_child_weigrt': 20,
+                  'silent': 1,
+                  'subsample':.9,
+                  'colsample_bytree': .85,
+                  #'eval_metric': 'auc',
+                  #'eval_metric': 'error',
+                  'seed': 1,
+                  'nthread': 1,
+                  'verbosity': 1,
+                  'early_stopping_rounds' : 10}
 
-    num_trees  = options.tree_number
-    gbm       = xgb.train(params, eventContainer.dtrain, num_trees)
+    # Actual training
+    gbm = xgb.train(params, eventContainer.dtrain, options.tree_number)
 
-    output = open(options.out_name+'_'+str(adds)+"/"+options.out_name+'_'+str(adds)+"_weights"+'.pkl', 'wb')
+    # Store BDT
+    output = open(options.out_name+'_'+str(adds)+'/' + \ # Own directory
+            options.out_name+'_'+str(bdt_num)+'_weights.pkl', 'wb')
     pkl.dump(gbm, output)
 
+    # Plot feature importances
     xgb.plot_importance(gbm)
-    plt.pyplot.savefig(options.out_name+'_'+str(adds)+"/"+options.out_name+'_'+str(adds)+'_fimportance.png', dpi=500, bbox_inches='tight', pad_inches=0.5)
-
-print("Files saved in: ", options.out_name+'_'+str(adds))
+    plt.pyplot.savefig(options.out_name+'_'+str(adds)+"/" + \ # BDT Directory
+            options.out_name+'_'+str(bdt_num)+'_fimportance.png', # png file name
+            dpi=500, bbox_inches='tight', pad_inches=0.5) # png parameters
+    
+    # Closing statment
+    print("Files saved in: ", options.out_name+'_'+str(adds))
